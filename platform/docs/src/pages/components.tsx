7b9faef0dd27f50fb685af15b78cc7bebb769ca9
mlokhandwala <lokhandwalamohammed@yahoo.com>
2025-12-22 19:21:27 +0530

    Some more points for branding on UI

--- platform/docs/src/pages/components.tsx
+++ platform/docs/src/pages/components.tsx
@@ -128,8 +128,8 @@ export default function ComponentShowcase() {
 
   return (
     <Layout
-      title="OHIF Viewer Design System"
-      description="OHIF Viewer Design System"
+      title="TrustTalk Viewer Design System"
+      description="TrustTalk Viewer Design System"
     >
       <div className="text-foreground min-h-screen bg-black">
         <div className="mx-auto my-4 max-w-5xl pt-6 pb-3">
